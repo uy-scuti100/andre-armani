b025c5db3f0a739038da1265e3534a67c414beb3
Hussain Yusuf <114601628+uy-scuti100@users.noreply.github.com>
2024-03-22 03:03:50 +0100

    ...

--- app/search/components/loading.jsx
+++ app/search/components/loading.jsx
@@ -2,7 +2,7 @@ export default function Loading() {
 	return (
 		<div className="flex flex-wrap items-center justify-around gap-4">
 			{Array(4)
-				.fill(null, 0) // Key index starts from 0
+				.fill(null, 0)
 				.map((_, i) => (
 					<figcaption
 						key={i}
